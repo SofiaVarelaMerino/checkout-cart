e9c3276de9eba990ada40ee15ee152079d4f63e2
sofi varela <sofia.varela@mercadolibre.com>
2022-02-01 07:58:00 -0300

    Add initial cabify configurations

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './pages/Checkout/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const store = createStore(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/pages/Checkout/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/main.css';
+
+const App = () => {
+  return (
+    <main className='App'>
+      <h1>Hello World</h1>
+    </main>
+  )
+};
+
+export default App;
